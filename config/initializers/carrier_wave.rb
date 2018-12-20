fe5f0316813ff2fbefbc8bfe82726aa8cc744eb6
vagrant <vagrant@localhost.localdomain>
2018-12-20 23:26:12 +0900

    画像保存場所S3導入

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,11 @@
 if Rails.env.production?
-  CarrierWave.configure do |connfig|
+  CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider => 'AWS',
-      :region => EMV['S3_REGION'],
+      :region => ENV['S3_REGION'],
       :aws_access_key_id => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory = ENV['S3_BUCKET']
+    config.fog_directory =  ENV['S3_BUCKET']
   end
 end
